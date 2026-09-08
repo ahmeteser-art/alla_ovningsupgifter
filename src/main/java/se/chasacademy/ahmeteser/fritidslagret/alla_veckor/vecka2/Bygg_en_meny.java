477fdbc18ae174bc8da6158896ca2a5add76a362
ahmeteser-art <ahmet.eser@student.chasacademy.se>
2026-09-08 23:50:17 +0200

    the vecka 2 updated

--- src/main/java/se/chasacademy/ahmeteser/fritidslagret/alla_veckor/vecka2/Bygg_en_meny.java
+++ src/main/java/se/chasacademy/ahmeteser/fritidslagret/alla_veckor/vecka2/Bygg_en_meny.java
@@ -17,4 +17,36 @@ public class Bygg_en_meny {
     användarinmatning
     Programmet ska fortsätta visa menyn tills användaren väljer alternativ 3.
      */
+    void main(){
+
+        int choice=0;
+
+        while(choice!=3){
+
+            IO.println("\nMeny:");
+            IO.println("1. Skriv ut en hälsning");
+            IO.println("2. Beräkna kvadraten av ett tal");
+            IO.println("3. Avsluta");
+
+            // Get user input
+            String input = IO.readln("Välj ett alternativ (1-3): ");
+            choice = Integer.parseInt(input);
+
+            switch (choice){
+                case 1 -> IO.println("hhey och welkommen!");
+                case 2 -> {
+
+                    String getNumber = IO.readln("please enter a number: ");
+                    int number = Integer.parseInt(getNumber);
+                    double square = number * number;
+                    IO.println("Kvadraten av " + number + " är: " + square);
+                }
+
+                case 3 -> IO.println("Avslutar programmet...");
+
+                default -> IO.println("Ogiltigt val. Försök igen.");
+            }
+
+        }
+    }
 }
